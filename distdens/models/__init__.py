335abcbc29dbed23bf99f5cf7e37fc4869db88c3
Efren Cabrera <efren507.507@gmail.com>
2019-08-05 11:43:41 -0700

    Agrega clase SamplingQuadrant al espacio de nombre del módulo

---
+++ distdens/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+Submódulo encargadod e contener las clases que representan las entidades involucradas
+en el calculo de los mapas de densidad
+"""
+
+from .SamplingQuadrat import SamplingQuadrat
