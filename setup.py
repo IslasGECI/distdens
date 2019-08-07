05d4d6e242f7623cf50721a4ea5d0697ca9f8242
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-08-07 13:32:33 -0700

    Agrega dependencia a geoambiental

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="distdens",
     version="0.1.0",
     packages=find_packages(),
-    install_requires = [        
+    install_requires = [
+        "geoambiental @ git+https://git@github.com/islasgeci/geoambiental@v0.1.0#egg=geoambiental-v0.1.0",
         "numpy",
         "pandas",
         "scipy",
