db6fec4009f150ba3b298158bfff4e6d1df4024c
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-08-07 13:55:10 -0700

    Corrige formato de archivos

--- distdens/models/__init__.py
+++ distdens/models/__init__.py
@@ -1,5 +1,5 @@
 """
-Submódulo encargadod e contener las clases que representan las entidades involucradas
+Submódulo encargado de contener las clases que representan las entidades involucradas
 en el calculo de los mapas de densidad
 """
 
