64353c12d701cf83b4c0a44a272371972f9e2941
Fernando <fernando.alvarez@islas.org.mx>
2019-10-15 10:45:02 -0700

    Agrega pruebas para QuadratTableReader

---
+++ distdens/tests/test_QuadratTableReader.py
@@ -0,0 +1,34 @@
+import unittest
+import pandas as pd
+from ..io import QuadratTableReader
+
+data = {'Especie': ['Synthliboramphus hypoleucus','Synthliboramphus hypoleucus','Especie2'], \
+'Sitio_o_colonia': ['Morro Prieto','Sitio2','Morro Prieto'], \
+'Area_del_cuadrante': [4,4,4], \
+'Madrigueras_con_actividad_aparente': [0,1,2], \
+'Este': [374448,374458,374542], \
+'Norte': [3198223,3198229,3198266],
+'Zona_utm':['11R','11R','11R']}
+
+datos=pd.DataFrame(data)
+
+filtro = {"Sitio_o_colonia": "Morro Prieto",
+          "Especie": "Synthliboramphus hypoleucus"}
+
+class TestQuadratTableReader(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Crea variables que se usarán en las pruebas
+        """
+        self.cuadrantes = QuadratTableReader.read(datos, filtro)
+
+    def test_get_density(self):
+        """
+        Verifica que la densidad de calcule de manera correcta
+        """
+        self.assertTrue(type(self.cuadrantes).__name__ == 'ndarray')
+
+
+if __name__ == '__main__':
+    unittest.main()
