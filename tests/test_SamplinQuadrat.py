59be5424e08e4f70d7619359429c7b18c54a761a
Nepo <braulio.rojas@islas.org.mx>
2020-05-19 11:25:01 -0700

    Caza mutantes de propiedades

--- tests/test_SamplinQuadrat.py
+++ tests/test_SamplinQuadrat.py
@@ -37,6 +37,16 @@ class TestGeoCircle(unittest.TestCase):
         """
         self.assertAlmostEqual(self.cuadrante.x, np.array(399086.97330437))
 
+    def test_y_is_a_property(self):
+        """
+        """
+        self.assertAlmostEqual(self.cuadrante.y, np.array(2765319.94402007))
+
+    def test_utm_zone_is_a_property(self):
+        """
+        """
+        self.assertTrue(np.array_equal(self.cuadrante.utm_zone, np.array(['11', 'R'], dtype='<U21')))
+
 
 if __name__ == '__main__':
     unittest.main()
