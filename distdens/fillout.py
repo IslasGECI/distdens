e09b6beb41620fe583def9e366b83adac766e810
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2020-06-25 17:47:02 -0700

    Formatea con Black

--- distdens/fillout.py
+++ distdens/fillout.py
@@ -4,15 +4,45 @@ import matplotlib.pyplot as plt
 
 def fillout(x, y, limits=None, **kwargs):
     if limits is None:
-        limits=[min(x), max(x), min(y), max(y)]
-    x_inicial = np.array(limits[0]).reshape(1,1)
-    x_final = np.array(limits[1]).reshape(1,1)
-    y_inicial = np.array(limits[2]).reshape(1,1)
-    y_final = np.array(limits[3]).reshape(1,1)
+        limits = [min(x), max(x), min(y), max(y)]
+    x_inicial = np.array(limits[0]).reshape(1, 1)
+    x_final = np.array(limits[1]).reshape(1, 1)
+    y_inicial = np.array(limits[2]).reshape(1, 1)
+    y_final = np.array(limits[3]).reshape(1, 1)
     indice_pegado_costa = np.where(x == min(x))[0][0]
-    x_cerrada = np.concatenate([x[indice_pegado_costa:].reshape(len(x[indice_pegado_costa:]), 1), x[0:indice_pegado_costa].reshape(len(x[0:indice_pegado_costa]), 1), x[indice_pegado_costa].reshape(1,1)])
-    y_cerrada = np.concatenate([y[indice_pegado_costa:].reshape(len(y[indice_pegado_costa:]), 1), y[0:indice_pegado_costa].reshape(len(y[0:indice_pegado_costa]), 1), y[indice_pegado_costa].reshape(1,1)])
-    x_prueba = np.concatenate([x_inicial,x_inicial,x_final,x_final,x_inicial,np.flip(x_cerrada.reshape(len(x_cerrada), 1))])
-    y_prueba = np.concatenate([y_inicial,y_final,y_final,y_inicial,y_inicial,np.flip(y_cerrada.reshape(len(x_cerrada), 1))])
+    x_cerrada = np.concatenate(
+        [
+            x[indice_pegado_costa:].reshape(len(x[indice_pegado_costa:]), 1),
+            x[0:indice_pegado_costa].reshape(len(x[0:indice_pegado_costa]), 1),
+            x[indice_pegado_costa].reshape(1, 1),
+        ]
+    )
+    y_cerrada = np.concatenate(
+        [
+            y[indice_pegado_costa:].reshape(len(y[indice_pegado_costa:]), 1),
+            y[0:indice_pegado_costa].reshape(len(y[0:indice_pegado_costa]), 1),
+            y[indice_pegado_costa].reshape(1, 1),
+        ]
+    )
+    x_prueba = np.concatenate(
+        [
+            x_inicial,
+            x_inicial,
+            x_final,
+            x_final,
+            x_inicial,
+            np.flip(x_cerrada.reshape(len(x_cerrada), 1)),
+        ]
+    )
+    y_prueba = np.concatenate(
+        [
+            y_inicial,
+            y_final,
+            y_final,
+            y_inicial,
+            y_inicial,
+            np.flip(y_cerrada.reshape(len(x_cerrada), 1)),
+        ]
+    )
     plt.fill(x_prueba, y_prueba, **kwargs)
     return x_prueba, y_prueba
--- distdens/io/QuadratTableReader.py
+++ distdens/io/QuadratTableReader.py
@@ -16,10 +16,20 @@ class QuadratTableReader(ABC):
     def _filter_table(quadrats_table: pd.DataFrame, filter_dict) -> pd.DataFrame:
         data = quadrats_table
         for filter_argument in filter_dict.keys():
-            data = data[data[f"{filter_argument}"]
-                        == filter_dict[filter_argument]]
+            data = data[data[f"{filter_argument}"] == filter_dict[filter_argument]]
         return data
 
     @staticmethod
     def _create_quadrats_list(data: pd.DataFrame):
-        return np.array([SamplingQuadrat(*utm.to_latlon(row.Este, row.Norte, int(row.Zona_utm[:-1]), row.Zona_utm[-1]), row.Area_del_cuadrante, row.Madrigueras_con_actividad_aparente) for _, row in data.iterrows()])        
+        return np.array(
+            [
+                SamplingQuadrat(
+                    *utm.to_latlon(
+                        row.Este, row.Norte, int(row.Zona_utm[:-1]), row.Zona_utm[-1]
+                    ),
+                    row.Area_del_cuadrante,
+                    row.Madrigueras_con_actividad_aparente,
+                )
+                for _, row in data.iterrows()
+            ]
+        )
