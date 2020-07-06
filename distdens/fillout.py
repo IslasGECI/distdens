4ee4e7906333e2c36502202ec1ee2739803d1a65
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2020-07-06 12:18:03 -0700

    Refactoriza creando función __close_coordinate

--- distdens/fillout.py
+++ distdens/fillout.py
@@ -10,20 +10,8 @@ def fillout(x, y, limits=None, **kwargs):
     y_inicial = np.array(limits[2]).reshape(1, 1)
     y_final = np.array(limits[3]).reshape(1, 1)
     indice_pegado_costa = np.where(x == min(x))[0][0]
-    x_cerrada = np.concatenate(
-        [
-            x[indice_pegado_costa:].reshape(len(x[indice_pegado_costa:]), 1),
-            x[0:indice_pegado_costa].reshape(len(x[0:indice_pegado_costa]), 1),
-            x[indice_pegado_costa].reshape(1, 1),
-        ]
-    )
-    y_cerrada = np.concatenate(
-        [
-            y[indice_pegado_costa:].reshape(len(y[indice_pegado_costa:]), 1),
-            y[0:indice_pegado_costa].reshape(len(y[0:indice_pegado_costa]), 1),
-            y[indice_pegado_costa].reshape(1, 1),
-        ]
-    )
+    closed_x = __close_coordinate(x, indice_pegado_costa)
+    closed_y = __close_coordinate(y, indice_pegado_costa)
     x_prueba = np.concatenate(
         [
             x_inicial,
@@ -31,7 +19,7 @@ def fillout(x, y, limits=None, **kwargs):
             x_final,
             x_final,
             x_inicial,
-            np.flip(x_cerrada.reshape(len(x_cerrada), 1)),
+            np.flip(closed_x.reshape(len(closed_x), 1)),
         ]
     )
     y_prueba = np.concatenate(
@@ -41,8 +29,18 @@ def fillout(x, y, limits=None, **kwargs):
             y_final,
             y_inicial,
             y_inicial,
-            np.flip(y_cerrada.reshape(len(x_cerrada), 1)),
+            np.flip(closed_y.reshape(len(closed_y), 1)),
         ]
     )
     plt.fill(x_prueba, y_prueba, **kwargs)
     return x_prueba, y_prueba
+
+def __close_coordinate(coordinate, indice_pegado_costa):
+    closed_coordinate = np.concatenate(
+        [
+            coordinate[indice_pegado_costa:].reshape(len(coordinate[indice_pegado_costa:]), 1),
+            coordinate[0:indice_pegado_costa].reshape(len(coordinate[0:indice_pegado_costa]), 1),
+            coordinate[indice_pegado_costa].reshape(1, 1),
+        ]
+    )
+    return closed_coordinate
