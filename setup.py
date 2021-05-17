bdeb6dc0be8a954c19a32ca1a2461e14fd015a43
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-05-17 14:52:17 -0700

    🎨 Format with Black

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name="distdens",
     version="0.1.0",
     packages=find_packages(),
-    install_requires = [
+    install_requires=[
+        "geoambiental @ git+https://git@github.com/islasgeci/geoambiental@v0.1.0",
         "numpy",
         "pandas",
         "scipy",
         "utm",
-        "geoambiental @ git+https://git@github.com/islasgeci/geoambiental@v0.1.0"
-    ]
+    ],
 )
