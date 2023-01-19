2231bddc4c854079707d564751d091b7f724d490
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-19 15:53:48 -0800

    🏷️ Change version

--- distdens/__init__.py
+++ distdens/__init__.py
@@ -2,5 +2,5 @@
 Módulo utilizado para calcular los mapas de densidad a partir de datos de muestro
 o busquedas exhaustivas.
 """
-__version__ = "0.2.1"
+__version__ = "1.0.0"
 from .fillout import fillout  # noqa
